db79418eb3630af127f6322ce296619639faf40a
John Jetmore <jj33@pobox.com>
2020-11-29 14:55:32 +0000

    specify the smtp tester on windows

--- testing/regressions/bin/runenv.bat
+++ testing/regressions/bin/runenv.bat
@@ -4,6 +4,7 @@
 set PATH=C:\Strawberry\perl\bin;%PATH%
 copy ..\..\swaks var\swaks.pl >NUL
 set TEST_SWAKS=var\swaks.pl
+set TEST_SERVER=../server/smtp-server.pl
 set PERL5LIB=lib\authen-ntlm-local
 rem set PAGER=more
 rem this can be really convenient when you have a ton of small changes to accept so you don't have to quit out of the pager
